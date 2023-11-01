db7e64652f0dede5760d590df7053448331a131a
33103 <574055869@qq.com>
2023-11-01 09:42:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.22345678.xyz": "https://registry-1.docker.io",
+  "quay.22345678.xyz": "https://quay.io",
+  "gcr.22345678.xyz": "https://gcr.io",
+  "k8s-gcr.22345678.xyz": "https://registry.k8s.io",
+  "k8s.22345678.xyz": "https://registry.k8s.io",
+  "ghcr.22345678.xyz": "https://ghcr.io",
+  "cloudsmith.22345678.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
